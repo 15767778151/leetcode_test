045eb41e7e050e4723cee31360ec3cfd1477d4c7
zhoushan <lizhenshan@linghit.com>
2025-11-18 19:00:18 +0800

    5-最长回文字串

--- leetcode/editor/cn/LongestPalindromicSubstring.java
+++ leetcode/editor/cn/LongestPalindromicSubstring.java
@@ -29,17 +29,61 @@
 //
 //<div><div>Related Topics</div><div><li>双指针</li><li>字符串</li><li>动态规划</li></div></div><br><div><li>👍 7884</li><li>👎 0</li></div>
 package src.main.java.leetcode.editor.cn;
-public class LongestPalindromicSubstring{
+
+public class LongestPalindromicSubstring {
     public static void main(String[] args) {
-         Solution solution = new LongestPalindromicSubstring().new Solution();
-         
+        Solution solution = new LongestPalindromicSubstring().new Solution();
+        String s="abba";
+        System.out.println(solution.longestPalindrome(s));
     }
+
     //leetcode submit region begin(Prohibit modification and deletion)
-class Solution {
-    public String longestPalindrome(String s) {
-        return "";
+    class Solution {
+        public String longestPalindrome(String s) {
+            int pre=0;
+            int after=s.length()-1;
+            String maxLongestString="";
+
+
+            if(s.length()==1){
+                return s;
+            }
+
+            while(pre<after){
+                int i=pre;
+                int j=after;
+
+                String currentLongestString="";
+                while(i<j){
+                    if(s.charAt(i)!=s.charAt(j)){
+
+                    }else{
+                        i++;
+
+                    }
+                    j--;
+
+                    if(s.charAt(i)==s.charAt(j)){
+                        currentLongestString=s.substring(i,j+1);
+                        i++;
+                        j--;
+                    }else{
+                        currentLongestString="";
+                        j--;
+                    }
+
+
+                }
+                if(currentLongestString.length()>maxLongestString.length()){
+                    maxLongestString=currentLongestString;
+                }
+
+                pre++;
+            }
+
+            return maxLongestString;
+        }
     }
-}
 //leetcode submit region end(Prohibit modification and deletion)
 
 }
